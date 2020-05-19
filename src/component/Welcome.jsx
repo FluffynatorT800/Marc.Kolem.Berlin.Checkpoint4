96a016111fb7659518740b4431de8c26b9cdcbb2
FluffynatorT800 <58825226+FluffynatorT800@users.noreply.github.com>
2020-05-19 13:02:26 +0200

    checkpoint front (coming soon)

---
+++ src/component/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import '../Css/Welcome.css'
+
+function Welcome() {
+    return (
+        <div className="Welcome">
+            <h1>Wild-Circus</h1>
+        </div>
+        );
+}
+
+export default Welcome;
